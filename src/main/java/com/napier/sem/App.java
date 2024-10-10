3e611bd66f62baa84f6c8725e87bae87b42c91b0
NikitaEdin <nikita.lanetsky.uk@gmail.com>
2024-10-10 16:45:23 +0100

    Added dependencies and JavaDoc comment

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,7 +1,11 @@
 package com.napier.sem;
 
-// Class to run the app.
 public class App {
+
+    /**
+     * Main app method to initialise the application.
+     * @param args launching parameters
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
